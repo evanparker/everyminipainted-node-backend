d3d68ab57b867931971118bc0bcdf4a9b0b75650
Evan Parker <parkevan@gmail.com>
2026-03-04 10:21:08 -0800

    Convert email sending to use postmark (postmarkapp.com)

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -2,53 +2,46 @@ const nodemailer = require("nodemailer");
 const handlebars = require("handlebars");
 const fs = require("fs");
 const path = require("path");
+const postmark = require("postmark");
 
-const sendEmail = async (email, subject, payload, template) => {
+const sendEmail = async (
+  email,
+  subject,
+  payload,
+  htmlTemplate,
+  txtTemplate
+) => {
   try {
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT || 465,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD // naturally, replace both with your real credentials or an application-specific password
-      }
-    });
+    const client = new postmark.ServerClient(
+      process.env.EMAIL_POSTMARK_SERVER_CLIENT
+    );
 
-    const source = fs.readFileSync(path.join(__dirname, template), "utf8");
-    const compiledTemplate = handlebars.compile(source);
-    const options = () => {
-      return {
-        from: process.env.FROM_EMAIL,
-        to: email,
-        subject: subject,
-        html: compiledTemplate(payload)
-      };
-    };
-
-    // Send email
-    transporter.sendMail(options(), (error, info) => {
-      if (error) {
-        return error;
-      } else {
-        return res.status(200).json({
-          success: true
-        });
-      }
-    });
+    const bodies = {};
+    if (htmlTemplate) {
+      const source = fs.readFileSync(
+        path.join(__dirname, htmlTemplate),
+        "utf8"
+      );
+      const compiledTemplate = handlebars.compile(source);
+      bodies.HtmlBody = compiledTemplate(payload);
+    }
+    if (txtTemplate) {
+      const source = fs.readFileSync(path.join(__dirname, txtTemplate), "utf8");
+      const compiledTemplate = handlebars.compile(source);
+      bodies.TextBody = compiledTemplate(payload);
+    }
+    if (bodies.HtmlBody || bodies.TextBody) {
+      client.sendEmail({
+        From: process.env.FROM_EMAIL,
+        To: email,
+        Subject: subject,
+        ...bodies,
+        MessageStream: "password-reset"
+      });
+    }
   } catch (error) {
     return error;
   }
 };
-
-/*
-Example:
-sendEmail(
-  "youremail@gmail.com,
-  "Email subject",
-  { name: "Eze" },
-  "./templates/layouts/main.handlebars"
-);
-*/
 
 module.exports = sendEmail;
